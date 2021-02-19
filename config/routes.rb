718999e9ab1817115bbae21b93a9894303e3a2ca
Santiago Almiron <sanalmiron@hotmail.com>
2021-02-19 12:27:23 -0300

    new actualizations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'dose/new'
   get 'dose/create'
   resources :cocktails, only: [:index, :create, :show, :create] do
-    resources :doses, only:[:new, :create]
+    resources :doses, only:[:new, :create, :destroy]
   end
   resources :doses, only: [:destroy]
 end
