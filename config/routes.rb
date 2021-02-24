d8377303161c29e08f42f708037fafaf3ea9e1d9
Santiago Almiron <sanalmiron@hotmail.com>
2021-02-24 18:32:29 -0300

    som other changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'dose/new'
-  get 'dose/create'
   root to: "cocktails#index"
   resources :cocktails, only: [:create, :show] do
-    resources :doses, only:[:new, :create, :destroy]
+    resources :doses, only:[:new, :create]
   end
   resources :doses, only: [:destroy]
 end
-
-#resources :ingredients, only []
